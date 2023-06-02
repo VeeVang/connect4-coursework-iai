86ca2a82a554c83d3c822af96b2db36c92b69d07
VeeVang <weiyi_chen@yeah.net>
2023-06-02 22:37:41 +0800

    modified valuefun

--- Strategy.cpp
+++ Strategy.cpp
@@ -14,7 +14,7 @@ int connect_3_next_step_score = 200;
 int connect_2_score = 100;
 int piece_score = 4;
 int max_depth = 6;
-int add_thres = 3;
+int add_thres = 2;
 int round = -1;
 int win_lose_confidence_coef = 1;
 
@@ -262,11 +262,13 @@ int evaluateBoardFromSelf(int* const* board, int M, int N, int self, int opponen
 			if (board[i][j] == self && board[i][j + 1] == self && board[i][j + 2] == self) {
 				if (Empty(board, M, N, i, j - 1, self)) {
 					value += connect_3_score;
-					value += connect_2_score * relu((add_thres - (top[j - 1] - i)));
+					if ((top[j - 1] - i) != 1)
+						value += connect_2_score * relu((add_thres - (top[j - 1] - i)));
 				}
 				if (Empty(board, M, N, i, j + 3, self)){
 					value += connect_3_score;
-					value += connect_2_score * relu((add_thres - (top[j + 3] - i)));
+					if ((top[j + 3] - i) != 1)
+						value += connect_2_score * relu((add_thres - (top[j + 3] - i)));
 				}
 			}
 		}
@@ -299,11 +301,13 @@ int evaluateBoardFromSelf(int* const* board, int M, int N, int self, int opponen
 			if (board[i][j] == self && board[i + 1][j] == self && board[i + 2][j] == self) {
 				if (Empty(board, M, N, i - 1, j, self)) {
 					value += connect_3_score;
-					value += connect_2_score * relu((add_thres - (top[j] - (i - 1))));
+					if ((top[j] - (i - 1)) != 1)
+						value += connect_2_score * relu((add_thres - (top[j] - (i - 1))));
 				}
 				if (Empty(board, M, N, i + 3, j, self)){
 					value += connect_3_score;
-					value += connect_2_score * relu((add_thres - (top[j] - (i + 3))));
+					if ((top[j] - (i + 3)) != 1)
+						value += connect_2_score * relu((add_thres - (top[j] - (i + 3))));
 				}
 			}
 		}
@@ -314,11 +318,13 @@ int evaluateBoardFromSelf(int* const* board, int M, int N, int self, int opponen
 			if (board[i][j] == self && board[i + 1][j + 1] == self && board[i + 2][j + 2] == self) {
 				if (Empty(board, M, N, i - 1, j - 1, self)) {
 					value += connect_3_score;
-					value += connect_2_score * relu((add_thres - (top[j - 1] - (i - 1))));
+					if ((top[j - 1] - (i - 1)) != 1)
+						value += connect_2_score * relu((add_thres - (top[j - 1] - (i - 1))));
 				}
 				if (Empty(board, M, N, i + 3, j + 3, self)){
 					value += connect_3_score;
-					value += connect_2_score * relu((add_thres - (top[j + 3] - (i + 3))));
+					if ((top[j + 3] - (i + 3)) != 1)
+						value += connect_2_score * relu((add_thres - (top[j + 3] - (i + 3))));
 				}
 			}
 		}
@@ -377,11 +383,13 @@ int evaluateBoardFromSelf(int* const* board, int M, int N, int self, int opponen
 			if (board[i][j] == self && board[i + 1][j - 1] == self && board[i + 2][j - 2] == self) {
 				if (Empty(board, M, N, i - 1, j + 1, self)) {
 					value += connect_3_score;
-					value += connect_2_score * relu((add_thres - (top[j + 1] - (i - 1))));
+					if ((top[j + 1] - (i - 1)) != 1)
+						value += connect_2_score * relu((add_thres - (top[j + 1] - (i - 1))));
 				}
 				if (Empty(board, M, N, i + 3, j - 3, self)) {
 					value += connect_3_score;
-					value += connect_2_score * relu((add_thres - (top[j - 3] - (i + 3))));
+					if ((top[j - 3] - (i + 3)) != 1)
+						value += connect_2_score * relu((add_thres - (top[j - 3] - (i + 3))));
 				}
 			}
 		}
