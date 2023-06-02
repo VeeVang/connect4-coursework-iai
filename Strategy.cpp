ae9a5a8c301e6241717f1ceaed5e1c7d2e39f670
VeeVang <weiyi_chen@yeah.net>
2023-06-02 18:35:06 +0800

    debug

--- Strategy.cpp
+++ Strategy.cpp
@@ -50,7 +50,7 @@ extern "C" Point* getPoint(const int M, const int N, const int* top, const int* 
 
 	/*--------------------²ÎÁ¿--------------------------*/
 	// Å¼Êý£¿
-	int depth = 7;
+	int depth = 6;
 	
 	int* modifiable_top = new int[N];
 	for (int j = 0; j < N; j++) {
