d078f17982d90e45314b99843bf0b977287f77ad
VeeVang <weiyi_chen@yeah.net>
2023-06-02 15:38:24 +0800

    checking evaluate

--- Strategy.cpp
+++ Strategy.cpp
@@ -47,7 +47,7 @@ extern "C" Point* getPoint(const int M, const int N, const int* top, const int* 
 	//Add your own code below
 
 	/*--------------------参量--------------------------*/
-	int depth = 10;
+	int depth = 1;
 	
 	int* modifiable_top = new int[N];
 	for (int j = 0; j < N; j++) {
@@ -257,6 +257,14 @@ int evaluateBoardFromSelf(int* const* board, int M, int N, int self, int opponen
 			}
 		}
 	}
+	cerr << "evaluating board value!..." << endl;
+	cerr << "board: " << endl;
+	for(int i=0; i<M; i++) {
+		for(int j=0; j<N; j++) //每行有m列
+			cerr<<board[i][j]<<" "; //输出第i行第j列的数
+		cerr<<endl; //每输出完一行，就输出一个换行符
+	}
+	cerr << "value: " << value << endl;
 	return value;
 };
 
