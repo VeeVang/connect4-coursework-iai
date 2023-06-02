bf3f7beb48503a24a6ac3fc969bac2d276be476a
VeeVang <weiyi_chen@yeah.net>
2023-06-02 14:47:32 +0800

    created declaration

--- Strategy.cpp
+++ Strategy.cpp
@@ -27,7 +27,7 @@ using namespace std;
 	output:
 		你的落子点Point
 */
-extern "C" __declspec(dllexport) Point* getPoint(const int M, const int N, const int* top, const int* _board,  const int lastX, const int lastY, const int noX, const int noY){
+extern "C" __declspec(dllexport) Point* getPoint(const int M, const int N, const int* top, const int* _board, const int lastX, const int lastY, const int noX, const int noY){
 	/*
 		不要更改这段代码
 	*/
@@ -99,6 +99,7 @@ extern "C" __declspec(dllexport) Point* getPoint(const int M, const int N, const
 
 
 	delete[] modifiable_top;
+
 	// --- User code ends ---
 
 	/*
--- Strategy.h
+++ Strategy.h
@@ -17,7 +17,17 @@ extern "C" void clearPoint(Point *p);
 
 void clearArray(int M, int N, int **board);
 
+bool EmptyOrSelf(int *const *board, int M, int N, int i, int j, int self);
+
+bool Empty(int *const *board, int M, int N, int i, int j, int self);
+
+int evaluateBoard(int *const *board, int M, int N, int self, int opponent);
+
 int evaluateBoardFromSelf(int *const *board, int M, int N, int self, int opponent);
+
+int isSpace(int j, int *const *board, const int *top, int noX, int noY);
+
+int alphaBeta(int **board, int depth, int alpha, int beta, bool maximizing, int M, int N, int last_x, int last_y, int *top, int noX, int noY);
 
 /*
 	添加你自己的辅助函数
